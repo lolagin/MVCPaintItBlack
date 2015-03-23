e1b5568a3e4e2ed414b63494ab50f7f16541a882
Jeffrey Rosenthal <jeffrey.rosenthal@gmail.com>
2015-03-23 00:02:17 -0700

    mvc up

---
+++ iPaintUrFaceLul/painterModel.h
@@ -0,0 +1,13 @@
+//
+//  painterModel.h
+//  iPaintUrFaceLul
+//
+//  Created by Jeffrey C Rosenthal on 3/22/15.
+//  Copyright (c) 2015 Jeffreycorp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface painterModel : NSObject
+
+@end
---
+++ iPaintUrFaceLul/painterModel.m
@@ -0,0 +1,13 @@
+//
+//  painterModel.m
+//  iPaintUrFaceLul
+//
+//  Created by Jeffrey C Rosenthal on 3/22/15.
+//  Copyright (c) 2015 Jeffreycorp. All rights reserved.
+//
+
+#import "painterModel.h"
+
+@implementation painterModel
+
+@end
